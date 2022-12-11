08d574ef2a84817895c51ac91c1176129edccc85
johnNaduer <naduer789@gmail.com>
2022-12-11 08:31:04 -0800

    update: I corrected my code with the betty standard

--- shell.c
+++ shell.c
@@ -3,6 +3,8 @@
 /**
  * main - Main function that calls functions in order to run a simple shell.
  * Return: 0 if success.
+ * @ac: attribute not used
+ * @av: input first argument
  */
 
 int main(int ac __attribute__((unused)), char **av)
@@ -10,14 +12,14 @@ int main(int ac __attribute__((unused)), char **av)
 	/*declare a variable to store user input */
 	char *lectura = NULL;
 	/* define a string of delimiters to use for tokenizing user input */
-	char *separador = {" \t\n"}; 
+	char *separador = {" \t\n"};
 	/* declare a variable to store the evaluated argument */
 	char *evaluar_argumento = NULL;
 	/* declare a variable to store the tokenized input */
 	char **argumentos = NULL;
 	/* set up a signal handler for when the user presses Ctrl+C */
 	signal(SIGINT, sig_handler);
-	
+
 	while (1)
 	{
 		lectura = cargar_texto();
@@ -27,18 +29,18 @@ int main(int ac __attribute__((unused)), char **av)
 			/*print the environment variables */
 			imprimir_env();
 			free(lectura);
-                        continue;
-		}								
-		
+			continue;
+		}
+
 		/* tokenize the user input */
 		argumentos = toukenizar_lectura(lectura, separador);
-		
+
 		/* evaluate the first argument (i.e. the command) */
 		evaluar_argumento = evaluar(argumentos[0]);
 		argumentos[0] = NULL;
 		argumentos[0] = evaluar_argumento;
 		evaluar_argumento = NULL;
-		
+
 		/* create a child process and execute the command */
 		proceso_hijo(lectura, argumentos, av);
 	}
