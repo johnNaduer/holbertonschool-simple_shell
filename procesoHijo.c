75ab53e8f155f9e366c5106d81bdb7dd2801e3f2
johnNaduer <naduer789@gmail.com>
2022-12-11 08:55:45 -0800

    update: corrected my code with the betty standard child process

--- procesoHijo.c
+++ procesoHijo.c
@@ -1,20 +1,17 @@
 #include "shell.h"
-
 /**
- * proceso_hijo -             Function that duplicates a process.
- * @evaluar_primer_argumento: Pointer to a pointer to a char.
+ * proceso_hijo - Function that duplicates a process.
+ * @argumentos: Pointer to a pointer to a char.
+ * @lectura: input char
+ * @av: input char
  * Return:                    Void.
  */
-
 void proceso_hijo(char *lectura, char **argumentos, char **av)
 {
-	
 	/* declare a variable to store the child process ID */
 	pid_t p_pid;
-	
 	/* declare a variable to store the status of the child process */
 	int status = 0;
-
 	/* create a child process */
 	p_pid = fork();
 
@@ -25,12 +22,11 @@ void proceso_hijo(char *lectura, char **argumentos, char **av)
 		{
 			/* if the command is not found, print an error message */
 			printf("bash: %s: command not found\n", av[0]);
-			
+
 			 /* exit the child process */
 			exit(EXIT_FAILURE);
 		}
 	}
-	
 	/* if fork() failed */
 	else if (p_pid < 0)
 	{
@@ -46,12 +42,7 @@ void proceso_hijo(char *lectura, char **argumentos, char **av)
 		if (WIFEXITED(status))
 			/* get the exit status of the child process */
 			WEXITSTATUS(status);
-		/**
-		free_argumentos(argumentos);
-		*/
 	}
-	
 	free_argumentos(argumentos);
-
 	free(lectura);
 }
