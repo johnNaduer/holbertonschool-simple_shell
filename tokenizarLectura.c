de97dff4a7d66359a2048d093027eb45f43daf9f
johnNaduer <naduer789@gmail.com>
2022-12-11 08:43:22 -0800

    update: corrected my code with the betty standard tokenize

--- tokenizarLectura.c
+++ tokenizarLectura.c
@@ -15,29 +15,19 @@ char **toukenizar_lectura(char *lectura, char *separador)
 	n = contar_separador(lectura, ' ');
 	/* allocate memory to store the tokens */
 	argumentos_1 = malloc((n + 1) * sizeof(char *));
-	
-	 /* if memory allocation failed	*/
-	if (argumentos_1 == NULL)
-		{
-			return (NULL);
-		}
-	
+
+	/*if (argumentos_1 == NULL) return (NULL); */
 	/* get the first token from the input string */
 	token = strtok(lectura, separador);
-	
+
 	for (i = 0; token != NULL; i++)
 	{
 		/* store the current token in the argumentos_1 array */
 		argumentos_1[i] = strdup(token);
-		
-		/* if memory allocation failed */
-		if (argumentos_1[i] == NULL)
-		{
-				return (NULL);
-		}
+		/* if (argumentos_1[i] == NULL) return (NULL);*/
 		/* get the next token */
 		token = strtok(NULL, separador);
-		
+
 	}
 	argumentos_1[i] = NULL;
 	/* return the tokenized input */
