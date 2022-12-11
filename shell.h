e6b22c793f98b7be712093290225bcf8e85043f5
johnNaduer <naduer789@gmail.com>
2022-12-11 08:12:58 -0800

    feat: new file header

---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Funciones */
+
+extern char **environ;
+
+char *cargar_texto();
+char **toukenizar_lectura(char *line, char *delims);
+void proceso_hijo(char *_line, char **_args, char **av);
+void sig_handler(int signo);
+int contar_separador(char *line, char sep);
+int validar_espacios(char *input);
+char *evaluar(char *exe);
+char *localizar_path();
+void imprimir_env(void);
+void free_argumentos(char **arg);
+
+#endif
