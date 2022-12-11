2b9c39fd5d8e12cebe6ac0ee8fad96f59cbd307d
johnNaduer <naduer789@gmail.com>
2022-12-11 08:11:33 -0800

    feat: new file tokenize

---
+++ tokenizarLectura.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+/**
+ * toukenizar_lectura - Function that tokenizes a string.
+ * @lectura:            A string.
+ * @separador:          An array containing chars.
+ * Return:              Returns a string but tokenized.
+ */
+char **toukenizar_lectura(char *lectura, char *separador)
+{
+	int i = 0, n = 0;
+	/* declare a variable to store the tokenized input */
+	char **argumentos_1 = NULL;
+	char *token = NULL;
+	/* count the number of tokens in the input */
+	n = contar_separador(lectura, ' ');
+	/* allocate memory to store the tokens */
+	argumentos_1 = malloc((n + 1) * sizeof(char *));
+	
+	 /* if memory allocation failed	*/
+	if (argumentos_1 == NULL)
+		{
+			return (NULL);
+		}
+	
+	/* get the first token from the input string */
+	token = strtok(lectura, separador);
+	
+	for (i = 0; token != NULL; i++)
+	{
+		/* store the current token in the argumentos_1 array */
+		argumentos_1[i] = strdup(token);
+		
+		/* if memory allocation failed */
+		if (argumentos_1[i] == NULL)
+		{
+				return (NULL);
+		}
+		/* get the next token */
+		token = strtok(NULL, separador);
+		
+	}
+	argumentos_1[i] = NULL;
+	/* return the tokenized input */
+	return (argumentos_1);
+}
