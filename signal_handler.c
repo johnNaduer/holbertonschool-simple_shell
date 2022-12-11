9a8104230fda8178ed8cb3179e7f02295ffb1287
johnNaduer <naduer789@gmail.com>
2022-12-11 08:48:33 -0800

    update: corrected my code with the betty standard signal_handler.c

--- signal_handler.c
+++ signal_handler.c
@@ -1,10 +1,11 @@
 #include "shell.h"
-
-
-
+/**
+* sig_handler - function is a signal handler
+* @s_num: input int s_num
+*/
 void sig_handler(int s_num)
 {
-	if ( s_num == SIGINT)
+	if (s_num == SIGINT)
 	{
 		write(1, "\n#cisfun$ ", 10);
 	}
