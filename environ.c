df8296ca197830246d7c0c5a280fadff7f2380cf
KevinYeff <kevinyeff94@gmail.com>
2022-12-11 12:36:53 -0500

    fix: Betty style fixed.

--- environ.c
+++ environ.c
@@ -1,12 +1,13 @@
 #include "shell.h"
-
-
+/**
+ * imprimir_env - Functions that prints the environmental variables.
+ */
 
 void imprimir_env(void)
 {
 	int i;
 
-	for (i=0; environ[i]!=NULL; i++)
+	for (i = 0; environ[i] != NULL; i++)
 	{
 		printf("%s\n", environ[i]);
 	}
