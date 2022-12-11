265469b6bf3b64753834000a80aa64ae72209d44
KevinYeff <kevinyeff94@gmail.com>
2022-12-11 17:11:17 -0500

    fix: Betty style fixed.

--- evaluar.c
+++ evaluar.c
@@ -8,20 +8,20 @@
 char *evaluar(char *primer_argumento)
 {
 	char *path_1 = NULL;
-	char separador_1[] = ":"; 
+	char separador_1[] = ":";
 	char *construir_path = NULL;
 	char **listas_de_path = NULL;
 	size_t i = 0, len_line = 0;
-	
+
 	/* declare a variable to store information about a file */
 	struct stat st;
 
 	/* get the value of the PATH environment variable */
 	path_1 = localizar_path();
-	
+
 	/* tokenize the PATH variable */
 	listas_de_path = toukenizar_lectura(path_1, separador_1);
-	
+
 	/* free the memory used to store the PATH variable */
 	free(path_1);
 
@@ -30,12 +30,11 @@ char *evaluar(char *primer_argumento)
 		len_line = (strlen(primer_argumento));
 		construir_path = malloc((strlen(listas_de_path[i]) + len_line +
 					 2) * sizeof(char));
-		
 		/* copy the current directory from the PATH variable to construir_path */
 		strcpy(construir_path, listas_de_path[i]);
 		strcat(construir_path, "/");
 		strcat(construir_path, primer_argumento);
-		
+
 		/* check if the file exists */
 		if (stat(construir_path, &st) == 0)
 		{
